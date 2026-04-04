98d9167948119558e9b0026f408c494bccd64716
ncinemdn <trishafrancinem@gmail.com>
2026-04-04 20:55:59 +0800

    Added Backend

---
+++ BackEnd/WebAPIwithDapper/API/Controllers/DeliveryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectName.Models;
+using ProjectName.Services;
+
+namespace API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DeliveryController : Controller
+	{
+		DeliveryService deliveryServices = new DeliveryService();
+
+		[HttpGet]
+		public ActionResult GetAll()
+		{
+			var book = deliveryServices.GetAll();
+			return Ok(book);
+		}
+
+		[HttpGet("{id}")]
+		public Delivery GetById(int id)
+		{
+			return deliveryServices.GetById(id);
+		}
+
+		[HttpPost]
+		public bool Add(Delivery c)
+		{
+			return deliveryServices.Add(c);
+		}
+
+		[HttpPut]
+		public bool Update(Delivery c)
+		{
+			return deliveryServices.Updatet(c);
+		}
+
+		[HttpDelete]
+		public bool Delete(int id)
+		{
+			return deliveryServices.Delete(id);
+		}
+
+	}
+}
---
+++ BackEnd/WebAPIwithDapper/ProjectName/Repositories/GenericRepository.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using ProjectName.Models;
+
+namespace ProjectName.Repositories
+{
+    public class GenericRepository<T>: IGenericRepository<T> where T : class
+    {
+        IDbConnection connection;
+        readonly string connectionString = "Server=LAPTOP-21JQHQ4T; Database=GabayHalamanDB; Trusted_Connection=True; MultipleActiveResultSets=true";
+        public GenericRepository()
+        {
+            connection = new SqlConnection(connectionString);
+
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            string tableName = GetTableName();
+
+            string query = $"SELECT * FROM {tableName}";
+            return connection.Query<T>(query);
+        }
+
+
+
+        public T GetbyId(int id)
+        {
+            string tableName = GetTableName();
+            string columns = GetColumnNames();
+            string values = GetColumnValues();
+
+            string query = $"SELECT * FROM {tableName} WHERE Id = @Id";
+
+            return connection.QueryFirstOrDefault<T>(query, new { Id = id });
+        }
+
+        public bool Add(T Entity)
+        {
+            string tableName = GetTableName();
+            string columns = GetColumnNames();
+            string values = GetColumnValues();
+
+            string query = $"INSERT INTO {tableName} ({columns}) VALUES ({values})";
+
+            int affectedRow = 0;
+            affectedRow = connection.Execute(query, Entity);
+            return affectedRow == 1;
+        }
+
+        public bool Update(T Entity)
+        {
+            string tableName = GetTableName();
+            string setClause = GetSetClause(Entity);
+            string query = $"UPDATE {tableName} SET {setClause} WHERE Id = @Id";
+
+            int affectedRow = 0;
+            affectedRow = connection.Execute(query, Entity);
+            return affectedRow == 1;
+        }
+
+        private string GetSetClause(T entity)
+        {
+            var properties = typeof(T).GetProperties()
+                .Where(p => p.Name != "Id");
+
+            var setClause = string.Join(", ", properties.Select(p => $"{p.Name} = @{p.Name}"));
+            return setClause;
+        }
+
+
+        public bool Delete(int id)
+        {
+            string tableName = GetTableName();
+            string query = $"DELETE FROM {tableName} WHERE Id = @Id";
+
+            int affectedRow = 0;
+            affectedRow = connection.Execute(query, new { Id = id });
+            return affectedRow == 1;
+        }
+
+        public string GetTableName()
+        {
+            string tableName = "";
+            var type = typeof(T);
+            var tableAttr = type.GetCustomAttribute<TableAttribute>();
+            if (tableAttr != null)
+            {
+                tableName = $"[{tableAttr.Name}]";
+            }
+            return tableName;
+        }
+
+        public string GetColumnNames(bool excludeKey = true)
+        {
+            var type = typeof(T);
+            var columns = string.Join(",", type.GetProperties()
+                .Where(p => !excludeKey || !p.IsDefined(typeof(KeyAttribute)))
+                .Select(p =>
+                {
+                    var columnAttr = p.GetCustomAttribute<ColumnAttribute>();
+                    return columnAttr != null ? columnAttr.Name : p.Name;
+                }));
+            return columns;
+        }
+
+        public string GetColumnValues(bool excludeKey = true)
+        {
+
+            var columnValues = typeof(T).GetProperties()
+                .Where(p => !excludeKey || p.GetCustomAttribute<KeyAttribute>() == null);
+            var values = string.Join(",", columnValues.Select(p =>
+            {
+                return $"@{p.Name}";
+            }));
+
+            return values;
+        }
+
+        /*IEnumerable<T> IGenericRepository<T>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<Book> GetAll()
+        {
+            return connection.Query<Book>(
+                "spBooks_GetAll",
+                commandType: CommandType.StoredProcedure);
+        }*/
+
+    }
+}
